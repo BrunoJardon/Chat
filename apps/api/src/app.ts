cf4461c038ce6eb0fcbd0b64e73820095d833018
Bruno Jardon <brunojardon3@gmail.com>
2026-07-26 23:00:04 -0300

    Add morgan to api

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,10 +1,13 @@
 import cors from "cors";
 import express, { type Express } from "express";
+import morgan from "morgan";
 
 import { corsOptions } from "./config/cors.js";
 
 const app: Express = express();
 
 app.use(cors(corsOptions));
+app.use(morgan('dev'));
+
 
 export default app;
